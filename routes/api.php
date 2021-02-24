deb223311980248b5978438132c738a59a3c007a
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-02-24 16:10:49 -0600

    configuration of the routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix'=>'books'], function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/',[BookController::class,'index']);
+    Route::post('/',[BookController::class,'store']);
+    Route::get('/{book}',[BookController::class,'show']);
+    Route::put('/{book}',[BookController::class,'update']);
+    Route::patch('/{book}',[BookController::class,'update']);
+    Route::delete('/{book}',[BookController::class,'destroy']);
+
+});
+
+Route::group(['prefix'=>'authors'], function () {
+
+    Route::get('/',[AuthorController::class,'index']);
+    Route::post('/',[AuthorController::class,'store']);
+    Route::get('/{author}',[AuthorController::class,'show']);
+    Route::put('/{author}',[AuthorController::class,'update']);
+    Route::patch('/{author}',[AuthorController::class,'update']);
+    Route::delete('/{author}',[AuthorController::class,'destroy']);
+
 });
