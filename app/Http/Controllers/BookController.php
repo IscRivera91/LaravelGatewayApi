18db2d48c93088fcecbc0ad3fc0171818699bff1
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-02-25 17:14:31 -0600

    verify the author to make a new instance of book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use App\Services\BookService;
@@ -18,13 +19,20 @@ class BookController extends Controller
     public $bookService;
 
     /**
+     * The service to consume the author service
+     * @var authorService
+     */
+    public $authorService;
+
+    /**
      * Create a new controller instance
      * 
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     /**
@@ -45,6 +53,7 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        $this->authorService->obtainAuthor($request->author_id);
         return $this->successResponse($this->bookService->createBook($request->all()),Response::HTTP_CREATED);
     }
 
@@ -68,6 +77,9 @@ class BookController extends Controller
      */
     public function update(Request $request, $book)
     {
+        if (isset($request->author_id)) {
+            $this->authorService->obtainAuthor($request->author_id);
+        }
         return $this->successResponse($this->bookService->editBook($request->all(), $book));
     }
 
